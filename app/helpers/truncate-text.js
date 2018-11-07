e6ee28c560f8e14cc07a1595bd3520748f50ac54
Sylvie Howton <sylviehowton@Sylvies-MacBook-Air-3.local>
2018-11-06 17:08:50 -0800

    put starred and unstarred into divs with same component

--- app/helpers/truncate-text.js
+++ app/helpers/truncate-text.js
@@ -2,7 +2,7 @@ import { helper } from "@ember/component/helper";
 
 export function truncateText(params /*, hash*/) {
   // return params;
-  return params[0].substring(0, 15) + "...";
+  return params[0].substring(0, 25) + "...";
 }
 
 export default helper(truncateText);
--- tests/integration/helpers/truncate-text-test.js
+++ tests/integration/helpers/truncate-text-test.js
@@ -1,17 +1,25 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
 
-module('Integration | Helper | truncate-text', function(hooks) {
+module("Integration | Helper | truncate-text", function(hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test("it renders", async function(assert) {
+    this.set("value", "25");
+    this.set("sign", "...");
 
-    await render(hbs`{{truncate-text inputValue}}`);
+    await render(hbs`{{truncate-text value sign=sign data-test="characters"}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(this.element.textContent.trim(), "25...");
+  });
+
+  test("truncated characters", async function(assert) {
+    this.set("length", "25");
+    await render(hbs`{{truncate-text length data-test="characters"}}`);
+
+    assert.dom('[data-test="characters"]').length(25);
   });
 });
