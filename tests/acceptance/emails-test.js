4f02e193b9233c6db3ad5ce8a6f7ae4591bb63f1
Sylvie Howton <sylviehowton@sylvies-air-3.home>
2018-11-10 16:24:02 -0800

    all tests passed

--- tests/acceptance/emails-test.js
+++ tests/acceptance/emails-test.js
@@ -1,5 +1,11 @@
 import { module, test } from "qunit";
-import { visit, currentURL, fillIn, click } from "@ember/test-helpers";
+import {
+  visit,
+  currentURL,
+  fillIn,
+  click,
+  pauseTest
+} from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
 import setupMirage from "ember-cli-mirage/test-support/setup-mirage";
 
@@ -49,34 +55,51 @@ module("Acceptance | emails", function(hooks) {
   test("testing trash", async function(assert) {
     server.create("email", { starred: false });
     server.create("email", { starred: false });
-    window.confirm = () => false;
+    window.confirm = () => true;
 
-    await visit("/emails/");
+    await visit("/");
+    assert.dom('[data-test="unstar-email"]').exists({ count: 2 });
+
+    await visit("/emails/1");
     await click('[data-test="delete-email"]');
+    await visit("/");
+    assert.dom('[data-test="unstar-email"]').exists({ count: 1 });
 
-    assert.dom('[data-test="delete-email"]').exists({ count: 0 });
+    await visit("/emails/2");
+    await click('[data-test="delete-email"]');
+    await visit("/");
+    assert.dom('[data-test="unstar-email"]').exists({ count: 0 });
   });
 
   test("testing compose email", async function(assert) {
-    server.create("email");
+    // assert.dom('[data-test="create-email"]').exists({ count: 0 });
+    await visit("/");
+    assert.dom('[data-test="unstar-email"]').exists({ count: 0 });
+    await click("#composeEmailButton");
 
-    assert.dom('[data-test="create-email"]').exists({ count: 0 });
+    // await visit("/emails/new");
 
-    await visit("/emails/new");
-
+    // this.set("#to", "jesus@gmail.com");
     await fillIn("#to", "jesus@gmail.com");
+    // await pauseTest();
     await fillIn("#subject", "Save Me");
-    await fillIn("#from", "mysoul@gmail.com");
+    await fillIn("#from", "andmysoul@gmail.com");
     await fillIn("#message", "From dying in this class.");
     await click('[data-test="publish-email"]');
 
-    assert.equal(currentURL(), "/emails/1");
-    assert.dom('[data-test="email-to"]').hasText("jesus@gmail.com");
-    assert.dom('[data-test="email-subject"]').hasText("Save Me");
-    assert.dom('[data-test="email-from"]').hasText("mysoul@gmail.com");
-    assert
-      .dom('[data-test="email-message"]')
-      .hasText("From dying in this class.");
-    assert.dom('[data-test="create-email"]').exists({ count: 1 });
+    assert.equal(currentURL(), "/");
+    assert.dom('[data-test="unstar-email"]').exists({ count: 1 });
+
+    // Test the server database
+    // http://www.ember-cli-mirage.com/docs/v0.2.x/acceptance-testing/
+    // server.db.emails[0]
+
+    // assert.dom('[data-test="email-to"]').hasText("jesus@gmail.com");
+    // assert.dom('[data-test="email-subject"]').hasText("Save Me");
+    // assert.dom('[data-test="email-from"]').hasText("andmysoul@gmail.com");
+    // assert
+    //   .dom('[data-test="email-message"]')
+    //   .hasText("From dying in this class.");
+    // assert.dom('[data-test="create-email"]').exists({ count: 1 });
   });
 });
